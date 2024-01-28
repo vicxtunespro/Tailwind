faa569b05c04ba93a2a1088b2455ef9dfd900a76
vicxtunespro <vicitug@gmail.com>
2024-01-28 14:12:47 +0000

    Project started

--- app/page.js
+++ app/page.js
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-center items-center">
-      <div className="bg-black text-white dark:text-red-400">
-        <p>Hello xtunes</p>
-      </div>
+    <main>
+      
     </main>
   );
 }
---
+++ components/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AiOutlineMenu } from "react-icons/ai";
+
+
+export default function MobileMenu() {
+  return (
+    <div className='w-screen h-12 bg-blue-600 dark:bg-slate-900 text-white pl-8 pr-8 flex justify-between items-center'>
+        <p>Logo</p>
+        <AiOutlineMenu/>
+    </div>
+  )
+}
