25393a2f8caf38376917a2ecc72a3a18fd39d015
vicxtunespro <vicitug@gmail.com>
2024-01-28 16:42:36 +0000

    hero-section done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      
+      <HeroSection/>
     </main>
   );
 }
--- components/nav.jsx
+++ components/nav.jsx
@@ -4,7 +4,7 @@ import { AiOutlineMenu } from "react-icons/ai";
 
 export default function MobileMenu() {
   return (
-    <div className='w-screen h-12 bg-blue-600 dark:bg-slate-900 text-white pl-8 pr-8 flex justify-between items-center'>
+    <div className='w-screen h-12 fixed top-0 bg-blue-600 dark:bg-slate-900 text-white pl-8 pr-8 flex justify-between items-center'>
         <p>Logo</p>
         <AiOutlineMenu/>
     </div>
