cd4a4ed9dcc9852379c8a4c4bbbb8b61fda2c47d
vicxtunespro <vicitug@gmail.com>
2024-01-28 13:28:22 +0000

    test2

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen flex justify-center items-center">
-      <div className="bg-black text-white">
+      <div className="bg-black text-white dark:text-red-400">
         <p>Hello xtunes</p>
       </div>
     </main>
