66c06fbba5feddf630362898b781ea344ab703c0
vicxtunespro <vicitug@gmail.com>
2024-01-29 15:32:15 +0000

    Dark mode config

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="w-screen min-h-screen dark:bg-slate-800">
       <HeroSection/>
       <SkillSection></SkillSection>
     </main>
--- components/skill-section.jsx
+++ components/skill-section.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Skill() {
   return (
-<div class="skill p-4">
+<div class="skill p-4 my-8">
   <div class="styled-title">
     <p class="text-3xl font-bold">What I Do</p>
     <div class="h-0.5 w-32 bg-slate-200 after:block after:h-full after:w-8 after:bg-blue-400 after:content-['']"></div>
@@ -11,30 +11,30 @@ export default function Skill() {
   <div class="activities my-8 flex flex-col md:flex-row flex-wrap">
     <div class="flex basis-1/2">
       <div class="icon h-16 min-w-16 flex justify-center items-center text-5xl rounded-lg bg-slate-200 lg:max-w-16"></div>
-      <div class="px-8 py-4">
-        <p class="mb-1 text-2xl font-semibold">Ecommerce</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nihil perspiciatis voluptate, dolores dignissimos accusamus placeat, tenetur ullam magni illo incidunt harum nemo rem iusto, fuga temporibus repellat voluptatem et.</p>
+      <div class="px-2 md:px-4 py-4">
+        <p class="mb-1 text-2xl dark:text-white font-semibold">Ecommerce</p>
+        <p class="dark:text:white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nihil perspiciatis voluptate, dolores dignissimos accusamus placeat, tenetur ullam magni illo incidunt harum nemo rem iusto, fuga temporibus repellat voluptatem et.</p>
       </div>
     </div>
     <div class="flex basis-1/2">
       <div class="icon h-16 min-w-16 flex justify-center items-center text-5xl rounded-lg bg-slate-200 lg:max-w-16"></div>
-      <div class="px-8 py-4">
-        <p class="mb-1 text-2xl font-semibold">Ecommerce</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nihil perspiciatis voluptate, dolores dignissimos accusamus placeat, tenetur ullam magni illo incidunt harum nemo rem iusto, fuga temporibus repellat voluptatem et.</p>
+      <div class="px-2 md:px-4 py-4">
+        <p class="mb-1 text-2xl dark:text-white font-semibold">Ecommerce</p>
+        <p class="dark:text:white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nihil perspiciatis voluptate, dolores dignissimos accusamus placeat, tenetur ullam magni illo incidunt harum nemo rem iusto, fuga temporibus repellat voluptatem et.</p>
       </div>
     </div>
     <div class="flex basis-1/2">
       <div class="icon h-16 min-w-16 flex justify-center items-center text-5xl rounded-lg bg-slate-200 lg:max-w-16"></div>
-      <div class="px-8 py-4">
-        <p class="mb-1 text-2xl font-semibold">Ecommerce</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nihil perspiciatis voluptate, dolores dignissimos accusamus placeat, tenetur ullam magni illo incidunt harum nemo rem iusto, fuga temporibus repellat voluptatem et.</p>
+      <div class="px-2 md:px-4 py-4">
+        <p class="mb-1 text-2xl dark:text-white font-semibold">Ecommerce</p>
+        <p class="dark:text:white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nihil perspiciatis voluptate, dolores dignissimos accusamus placeat, tenetur ullam magni illo incidunt harum nemo rem iusto, fuga temporibus repellat voluptatem et.</p>
       </div>
     </div>
     <div class="flex basis-1/2">
       <div class="icon h-16 min-w-16 flex justify-center items-center text-5xl rounded-lg bg-slate-200 lg:max-w-16"></div>
-      <div class="px-8 py-4">
-        <p class="mb-1 text-2xl font-semibold">Ecommerce</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nihil perspiciatis voluptate, dolores dignissimos accusamus placeat, tenetur ullam magni illo incidunt harum nemo rem iusto, fuga temporibus repellat voluptatem et.</p>
+      <div class="px-2 md:px-4 py-4">
+        <p class="mb-1 text-2xl dark:text-white font-semibold">Ecommerce</p>
+        <p class="dark:text:white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nihil perspiciatis voluptate, dolores dignissimos accusamus placeat, tenetur ullam magni illo incidunt harum nemo rem iusto, fuga temporibus repellat voluptatem et.</p>
       </div>
     </div>
   </div>
